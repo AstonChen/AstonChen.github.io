d254388b9fe381e65e8a7268b888c5a61bbb8116
Aston <Aston@AstondeMacBook-Pro.local>
2016-10-03 05:26:42 +0800

    Site updated: 2016-10-03 05:26:42

--- js/totop.js
+++ js/totop.js
@@ -17,6 +17,6 @@
 
     // Scroll to top animation on click
     $(scrollElem).click(function(){
-        $('html, body').animate({scrollTop:0}, scrollSpeed); return false;
+        $('.body-wrap').animate({scrollTop:0}, scrollSpeed); return false;
     });
 })(jQuery);
