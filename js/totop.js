127d0b5163d4b6dd889b8d1a223ab4ed2e1f1666
Aston <Aston@AstondeMacBook-Pro.local>
2016-10-06 22:38:53 +0800

    Site updated: 2016-10-06 22:38:53

--- js/totop.js
+++ js/totop.js
@@ -12,7 +12,12 @@
     // Show and hide the scroll to top link based on scroll position
     scrollElem.hide();
     $(window).scroll(function () {
-        scrollElem.show();
+        var scrollTop = $(document).scrollTop();
+        if ( scrollTop > upperLimit ) {
+            $(scrollElem).stop().fadeTo(300, 1); // fade back in
+        }else{
+            $(scrollElem).stop().fadeTo(300, 0); // fade out
+        }
     });
 
     // Scroll to top animation on click
